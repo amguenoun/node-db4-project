5ba2c2a39aecf5aa58dc58f8d3311877413adaee
Ahmad Guenoun <amguenoun@gmail.com>
2020-01-17 11:31:47 -0500

    exposed endpoint to get instructions

--- resources/recipes/recipesController.js
+++ resources/recipes/recipesController.js
@@ -23,3 +23,17 @@ exports.getShoppingList = (req, res) => {
             res.status(500).json({ message: 'Failed to get shopping list for recipe' })
         })
 }
+
+exports.getInstructions = (req, res) => {
+    const recipeId = req.params.id;
+
+    db('recipes')
+        .select('instructions')
+        .where("id", recipeId)
+        .then(recipes => {
+            res.json(recipes);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get recipe instructions' })
+        })
+}
--- resources/recipes/recipesRouter.js
+++ resources/recipes/recipesRouter.js
@@ -9,4 +9,6 @@ router.get('/all', recipesController.getRecipes)
 
 router.get('/list/:id', recipesController.getShoppingList)
 
+router.get('/instructions/:id', recipesController.getInstructions)
+
 module.exports = router
